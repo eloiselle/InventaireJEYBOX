bf01a5819079332f6a5a1f36c3a1bba8e32db7e9
Incheap <Incheap@users.noreply.github.com>
2020-04-23 15:56:56 -0400

    nav reservation

--- consulter_reservation.php
+++ consulter_reservation.php
@@ -9,12 +9,12 @@
   </head>
   <body>
     <nav>
-      <ul>
-        <li><img src="images/JEYBOX_Logo_1037x300.png" alt="Logo JEYBOX" width="173" height="50"></li>
-        <li><a class="active" href="#categorie">Catégories</a></li>
-        <li><a href="#articles">Articles</a></li>
-        <li><a href="#reservation">Réservations</a></li>
-        <li style="float:right"><a href="#compte">Mon compte</a></li>
+      <ul class="menuUl">
+        <li class="menuLi"><img class="imgLogo" src="images/JEYBOX_Logo_1037x300.png" alt="Logo JEYBOX" width="173" height="50"></li>
+        <li class="menuLi"><a class="menuButton" href="#categorie">Catégories</a></li>
+        <li class="menuLi"><a class="menuButton" href="#articles">Articles</a></li>
+        <li class="menuLi"><a class="menuButton" href="#reservation">Réservations</a></li>
+        <li style="float:right"><a class="menuButton" href="#compte">Mon compte</a></li>
       </ul>
     </nav>
     <br>

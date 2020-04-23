f1147031b953cbaf7b1aff5c36867cf2863d08ca
DeltaHit <12175927+DeltaHit@users.noreply.github.com>
2020-04-23 17:10:40 -0400

    Forgot to add nav references in categorie.php

--- categorie.php
+++ categorie.php
@@ -11,9 +11,9 @@
     <nav>
       <ul class="menuUl">
         <li class="menuLi"><img class="imgLogo" src="images/JEYBOX_Logo_1037x300.png" alt="Logo JEYBOX" width="173" height="50"></li>
-        <li class="menuLi"><a class="menuButton" href="#categorie">Catégories</a></li>
-        <li class="menuLi"><a class="menuButton" href="#articles">Articles</a></li>
-        <li class="menuLi"><a class="menuButton" href="#reservation">Réservations</a></li>
+        <li class="menuLi"><a class="menuButton" href="categorie.php">Catégories</a></li>
+        <li class="menuLi"><a class="menuButton" href="article.php">Articles</a></li>
+        <li class="menuLi"><a class="menuButton" href="consulter_reservation.php">Réservations</a></li>
         <li style="float:right"><a class="menuButton" href="#compte">Mon compte</a></li>
       </ul>
     </nav>

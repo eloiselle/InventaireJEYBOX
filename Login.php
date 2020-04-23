912a148eeeb195d838538731cfb38c07decec1d0
Incheap <Incheap@users.noreply.github.com>
2020-04-23 15:18:01 -0400

    Login/Password/css update and reservations window

---
+++ Login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/Style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <title>S'authentifier</title>
+  </head>
+  <body>
+<main>
+  <article class="paddingTop">
+    <img src="images/small_logo.png" class="logo center" alt="Jey Box">
+  </article>
+  <div class="window">
+    <form class="login" action="index.html" method="get">
+      <label class="title">Connexion</label>
+      <br>
+      <br>
+      <hr>
+      <br>
+      <label for="txtuser" class="labelLeft customPadding1">Utilisateur</label><br>
+      <input id="txtuser" type="text" name="txtuser" value="" placeholder="Admin" class="txtbox" required>
+      <br>
+      <br>
+      <label for="txtpass" class="labelLeft customPadding2">Mot de passe</label><br>
+      <input id="txtpass" type="password" name="txtpass" value="" class="txtbox">
+      <br>
+      <br>
+      <br>
+      <input type="submit" name="btnlogin" class="btnLog" value="Se connecter">
+      <br>
+      <br>
+      <a href="ForgotPass.php" title = "Récupérez votre mot de passe" class="forgot">Mot de passe oublié ?</a>
+    </form>
+
+  </div>
+</main>
+  </body>
+</html>
---
+++ forgotPass.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/Style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <script src="Script/script.js"></script>
+    <title>Mot de passe oublié</title>
+  </head>
+  <body>
+<main>
+  <article class="paddingTop">
+    <img src="images/small_logo.png" class="logo center" alt="Jey Box">
+  </article>
+  <div class="window">
+    <form class="login" action="#" method="post">
+      <h1>Mot de passe oublié</h1>
+      <hr>
+      <label for="txtmail" class="labelLeft customPadding3">Courriel</label><br>
+      <input id="txtmail" type="text" name="txtmail" value="" class="txtbox" placeholder="exemple@hotmail.com" autocomplete="off" required>
+      <br>
+      <br>
+      <label for="txtpassword" class="labelLeft customPadding4">Nouveau mot de passe</label><br>
+      <input id="txtpassword" type="password" name="txtpassword" value="" placeholder="" class="txtbox" autocomplete="off" required>
+      <br>
+      <br>
+      <label for="txtconfirm" class="labelLeft customPadding5">Confirmer le mot de passe</label><br>
+      <input id="txtconfirm" type="password" name="txtconfirm" value="" class="txtbox" autocomplete="off" required>
+      <br>
+      <br>
+      <br>
+      <br>
+      <input type="submit" name="btnsend" class="btnLog" value="Envoyer" onclick="confirmMail(); confirmPass()">
+      <br>
+      <br>
+    </form>
+asdasdasd
+  </div>
+</main>
+  </body>
+</html>

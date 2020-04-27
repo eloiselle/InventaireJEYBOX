37de3e613f2568583c73ac8aa4dc38099efefe13
Incheap <Incheap@users.noreply.github.com>
2020-04-27 14:55:33 -0400

    icon

--- consulter_article.php
+++ consulter_article.php
@@ -3,13 +3,9 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/style.css" />
-<<<<<<< HEAD:article.php
     <!-- Allows to find search icon for search bar -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>index</title>
-=======
-    <title>JEYBOX - Articles</title>
->>>>>>> master:consulter_article.php
   </head>
   <body>
     <nav>

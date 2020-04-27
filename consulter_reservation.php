b0d033371fcb419a6b1df6b304fcf893ef9017b9
Yannick Plourde <yann62plourde@gmail.com>
2020-04-27 15:34:35 -0400

    changement apporté

--- consulter_reservation.php
+++ consulter_reservation.php
@@ -11,7 +11,6 @@
     <nav>
       <ul>
         <li><a href="index.html"><img src="images/JEYBOX_Logo_1037x300.png" alt="Logo JEYBOX" width="173" height="50"></a></li>
-        <li><a class="button" href="consulter_categorie.php">Catégories</a></li>
         <li><a class="button" href="consulter_article.php">Articles</a></li>
         <li><a class="active_button" href="consulter_reservation.php">Réservations</a></li>
         <li style="float:right"><a class="button" href="#compte">Mon compte</a></li>

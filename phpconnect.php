ce10c2fdd5befd5433c5bb188b7f1a5f62ac13f3
DeltaHit <12175927+DeltaHit@users.noreply.github.com>
2020-04-29 09:22:54 -0400

    Removed log comments

---
+++ phpconnect.php
@@ -0,0 +1,41 @@
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Test PHP Connection Script</title>
+  </head>
+  <body>
+    <h3>Welcome to the PHP Connect Test</h3>
+
+    <?php
+
+    $servername = 'localhost';
+    $username = 'deltahit';
+    $password = '';
+    $dbname = 'inventaire_jeybox';
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+       die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT id_etat, nom, description FROM etat";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+       // output data of each row
+       while($row = $result->fetch_assoc()) {
+           echo "id: " . $row["id_etat"]. " - Nom: " . $row["nom"]. " - Description: " . $row["description"]. "<br>";
+       }
+    } else {
+       echo "0 results";
+    }
+    $conn->close();
+    ?>
+  </body>
+</html>

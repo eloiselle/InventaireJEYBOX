c19831ebcb8c09c67a93a44cdb2f55decc535158
DeltaHit <12175927+DeltaHit@users.noreply.github.com>
2020-05-01 15:23:52 -0400

    Select table using id

--- 3_acces_bd/moteur_requetes_bd.php
+++ 3_acces_bd/moteur_requetes_bd.php
@@ -11,11 +11,15 @@
       $this->mySqlManager = new sqlManager();
     }
 
+    // ==============================================
+    //                    SELECTORS
+    // ==============================================
+
     // Article
     public function selectArticleFromID($id_article){
       $this->mySqlManager->init_connection();
 
-      $sql = "SELECT * FROM article WHERE id_article = " . $id_article;
+      $sql = 'SELECT * FROM article WHERE id_article = ' . $id_article;
       $result = $this->mySqlManager->get_connection()->query($sql);
 
       $this->mySqlManager->get_connection()->close();
@@ -26,7 +30,7 @@
     public function selectCategorieFromID($id_categorie){
       $this->mySqlManager->init_connection();
 
-      $sql = "SELECT * FROM categorie WHERE id_categorie = " . $id_categorie;
+      $sql = 'SELECT * FROM categorie_article WHERE id_categorie = ' . $id_categorie;
       $result = $this->mySqlManager->get_connection()->query($sql);
 
       $this->mySqlManager->get_connection()->close();
@@ -37,7 +41,7 @@
     public function selectContactUrgenceFromID($id_contact_urgence){
       $this->mySqlManager->init_connection();
 
-      $sql = "SELECT * FROM contact_urgence WHERE id_contact_urgence = " . $id_contact_urgence;
+      $sql = 'SELECT * FROM contact_urgence WHERE id_contact_urgence = ' . $id_contact_urgence;
       $result = $this->mySqlManager->get_connection()->query($sql);
 
       $this->mySqlManager->get_connection()->close();
@@ -48,7 +52,7 @@
     public function selectEtatFromID($id_etat){
       $this->mySqlManager->init_connection();
 
-      $sql = "SELECT * FROM etat WHERE id_etat = " . $id_etat;
+      $sql = 'SELECT * FROM etat WHERE id_etat = ' . $id_etat;
       $result = $this->mySqlManager->get_connection()->query($sql);
 
       $this->mySqlManager->get_connection()->close();
@@ -59,7 +63,7 @@
     public function selectPermissionFromID($id_permission){
       $this->mySqlManager->init_connection();
 
-      $sql = "SELECT * FROM permission WHERE id_permission = " . $id_permission;
+      $sql = 'SELECT * FROM permission WHERE id_permission = ' . $id_permission;
       $result = $this->mySqlManager->get_connection()->query($sql);
 
       $this->mySqlManager->get_connection()->close();
@@ -70,7 +74,7 @@
     public function selectReferenceFromID($id_reference){
       $this->mySqlManager->init_connection();
 
-      $sql = "SELECT * FROM reference WHERE id_reference = " . $id_reference;
+      $sql = 'SELECT * FROM reference WHERE id_reference = ' . $id_reference;
       $result = $this->mySqlManager->get_connection()->query($sql);
 
       $this->mySqlManager->get_connection()->close();
@@ -81,7 +85,7 @@
     public function selectReservationFromID($id_reservation){
       $this->mySqlManager->init_connection();
 
-      $sql = "SELECT * FROM reservation WHERE id_reservation = " . $id_reservation;
+      $sql = 'SELECT * FROM reservation WHERE id_reservation = ' . $id_reservation;
       $result = $this->mySqlManager->get_connection()->query($sql);
 
       $this->mySqlManager->get_connection()->close();
@@ -92,7 +96,7 @@
     public function selectSousCategorieArticleFromID($id_sous_categorie){
       $this->mySqlManager->init_connection();
 
-      $sql = "SELECT * FROM sous_categorie_article WHERE id_sous_categorie = " . $id_sous_categorie;
+      $sql = 'SELECT * FROM sous_categorie_article WHERE id_sous_categorie = ' . $id_sous_categorie;
       $result = $this->mySqlManager->get_connection()->query($sql);
 
       $this->mySqlManager->get_connection()->close();
@@ -103,7 +107,110 @@
     public function selectUtilisateurFromID($nom_utilisateur){
       $this->mySqlManager->init_connection();
 
-      $sql = "SELECT * FROM utilisateur WHERE nom_utilisateur = " . $nom_utilisateur;
+      $sql = 'SELECT * FROM utilisateur WHERE nom_utilisateur = ' . $nom_utilisateur;
+      $result = $this->mySqlManager->get_connection()->query($sql);
+
+      $this->mySqlManager->get_connection()->close();
+      return $result;
+    }
+
+    // ==============================================
+    //                    CREATORS
+    // ==============================================
+
+    // Article
+    public function createArticleFromArray($array_article){
+      $this->mySqlManager->init_connection();
+
+      $sql = 'INSERT INTO article (id_article, nom, fiche_url, id_sous_categorie, id_etat) VALUES ()' ;
+      $result = $this->mySqlManager->get_connection()->query($sql);
+
+      $this->mySqlManager->get_connection()->close();
+      return $result;
+    }
+
+    // Categorie
+    public function createCategorieFromArray($array_categorie){
+      $this->mySqlManager->init_connection();
+
+      $sql = 'INSERT INTO categorie_article (nom, description) VALUES ( "' . $array_categorie["nom"] . '", "' . $array_categorie["description"] . '")';
+      $result = $this->mySqlManager->get_connection()->query($sql);
+
+      $this->mySqlManager->get_connection()->close();
+      return $result;
+    }
+
+    // Contact Urgence
+    public function createContactUrgenceFromArray($array_contact_urgence){
+      $this->mySqlManager->init_connection();
+
+      $sql = 'SELECT * FROM contact_urgence WHERE id_contact_urgence = ' . $id_contact_urgence;
+      $result = $this->mySqlManager->get_connection()->query($sql);
+
+      $this->mySqlManager->get_connection()->close();
+      return $result;
+    }
+
+    // Etat
+    public function createEtatFromArray($array_etat){
+      $this->mySqlManager->init_connection();
+
+      $sql = 'SELECT * FROM etat WHERE id_etat = ' . $id_etat;
+      $result = $this->mySqlManager->get_connection()->query($sql);
+
+      $this->mySqlManager->get_connection()->close();
+      return $result;
+    }
+
+    // Permission
+    public function createPermissionFromArray($array_permission){
+      $this->mySqlManager->init_connection();
+
+      $sql = 'SELECT * FROM permission WHERE id_permission = ' . $id_permission;
+      $result = $this->mySqlManager->get_connection()->query($sql);
+
+      $this->mySqlManager->get_connection()->close();
+      return $result;
+    }
+
+    // Reference
+    public function createReferenceFromArray($array_reference){
+      $this->mySqlManager->init_connection();
+
+      $sql = 'SELECT * FROM reference WHERE id_reference = ' . $id_reference;
+      $result = $this->mySqlManager->get_connection()->query($sql);
+
+      $this->mySqlManager->get_connection()->close();
+      return $result;
+    }
+
+    // Reservation
+    public function createReservationFromArray($array_reservation){
+      $this->mySqlManager->init_connection();
+
+      $sql = 'SELECT * FROM reservation WHERE id_reservation = ' . $id_reservation;
+      $result = $this->mySqlManager->get_connection()->query($sql);
+
+      $this->mySqlManager->get_connection()->close();
+      return $result;
+    }
+
+    // Sous Catégorie Article
+    public function createSousCategorieArticleFromArray($array_sous_categorie_article){
+      $this->mySqlManager->init_connection();
+
+      $sql = 'SELECT * FROM sous_categorie_article WHERE id_sous_categorie = ' . $id_sous_categorie;
+      $result = $this->mySqlManager->get_connection()->query($sql);
+
+      $this->mySqlManager->get_connection()->close();
+      return $result;
+    }
+
+    // Utilisateur
+    public function createUtilisateurFromArray($array_utilisateur){
+      $this->mySqlManager->init_connection();
+
+      $sql = 'SELECT * FROM utilisateur WHERE nom_utilisateur = ' . $nom_utilisateur;
       $result = $this->mySqlManager->get_connection()->query($sql);
 
       $this->mySqlManager->get_connection()->close();
--- 3_acces_bd/php_main.php
+++ 3_acces_bd/php_main.php
@@ -9,16 +9,102 @@
       include 'moteur_requetes_bd.php';
 
       $moteurRequetes = new moteurRequetesBD();
-      $result = $moteurRequetes->selectEtatFromID(1);
+      ?>
 
-      if ($result->num_rows > 0) {
-         // output data of each row
-         while($row = $result->fetch_assoc()) {
-             echo "id: " . $row["id_etat"]. " - Nom: " . $row["nom"]. " - Description: " . $row["description"]. "<br>";
-         }
-      } else {
-         echo "0 results";
+      <!-- Test for categorie_article -->
+      <form method="post">
+        <label for="desired_table">Table: </label>
+        <select id="desired_table" name="desired_table">
+          <option value="article">Article</option>
+          <option value="categorie">Catégorie article</option>
+          <option value="sous_categorie_article">Sous catégorie article</option>
+          <option value="etat">État</option>
+          <option value="reference">Réference</option>
+          <option value="utilisateur">Utilisateur</option>
+          <option value="permission">Permission</option>
+          <option value="contact_urgence">Contact urgence</option>
+          <option value="reservation">Réservation</option>
+        </select>
+        <label for="fid">id: </label>
+        <input type="number" id="fid" name="desired_id" value="1" min='1'> <br>
+        <input type="submit" name="select" value="Select from ID"/>
+      </form>
+
+      <?php
+      if(isset($_POST['desired_id'])) {
+        $id = $_POST['desired_id']; //its now in a varible
       }
-     ?>
+
+      if(isset($_POST['desired_table'])) {
+        $table = $_POST['desired_table']; //its now in a varible
+      }
+
+      if (isset($_POST['select'])) {
+        switch ($table) {
+          case 'article':
+            $result = $moteurRequetes->selectArticleFromID($id);
+            break;
+
+          case 'categorie':
+            $result = $moteurRequetes->selectCategorieFromID($id);
+            break;
+
+          case 'sous_categorie_article':
+            $result = $moteurRequetes->selectSousCategorieArticleFromID($id);
+            break;
+
+          case 'contact_urgence':
+            $result = $moteurRequetes->selectContactUrgenceFromID($id);
+            break;
+
+          case 'etat':
+            $result = $moteurRequetes->selectEtatFromID($id);
+            break;
+
+          case 'reference':
+            $result = $moteurRequetes->selectReferenceFromID($id);
+            break;
+
+          case 'utilisateur':
+            $result = $moteurRequetes->selectUtilisateurFromID($id);
+            break;
+
+          case 'permission':
+            $result = $moteurRequetes->selectPermissionFromID($id);
+            break;
+
+          case 'contact_urgence':
+            $result = $moteurRequetes->selectContactUrgenceFromID($id);
+            break;
+
+          case 'reservation':
+            $result = $moteurRequetes->selectReservationFromID($id);
+            break;
+
+          default:
+            // code...
+            break;
+        }
+        if ($result->num_rows > 0) {
+           // output data of each row
+           while($row = $result->fetch_assoc()) {
+               print_r($row);
+           }
+        } else {
+           echo "0 results";
+        }
+      }
+
+      // Insert into using PHP and form (unused at this time)
+      if (isset($_POST['insert'])) {
+        $categorieArray = [
+          "nom" => "Composante électronique",
+          "description" => "Objets électroniques variés pouvant servir à équiper un système d'autres sous-systèmes.",
+        ];
+
+        $moteurRequetes->createCategorieFromArray($categorieArray);
+      }
+
+      ?>
   </body>
 </html>

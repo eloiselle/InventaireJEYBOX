bc4c59a00533f3c6208d7b1a07ab36a34ebba5f9
DeltaHit <12175927+DeltaHit@users.noreply.github.com>
2020-05-01 04:19:52 -0400

    Added methods to get every object in the database into moteur_requetes_bd.php

--- 3_acces_bd/php_main.php
+++ 3_acces_bd/php_main.php
@@ -6,23 +6,19 @@
   </head>
   <body>
     <?php
-      include 'gestionnaire_connexion_bd.php';
+      include 'moteur_requetes_bd.php';
 
-      $mySqlManager = new sqlManager();
-      $mySqlManager->init_connection();
-
-      $sql = "SELECT id_etat, nom, description FROM etat";
-      $result = $mySqlManager->get_connection()->query($sql);
+      $moteurRequetes = new moteurRequetesBD();
+      $result = $moteurRequetes->selectEtatFromID(1);
 
       if ($result->num_rows > 0) {
-           // output data of each row
-           while($row = $result->fetch_assoc()) {
-               echo "id: " . $row["id_etat"]. " - Nom: " . $row["nom"]. " - Description: " . $row["description"]. "<br>";
-           }
-        } else {
-           echo "0 results";
-        }
-        $mySqlManager->get_connection()->close();
+         // output data of each row
+         while($row = $result->fetch_assoc()) {
+             echo "id: " . $row["id_etat"]. " - Nom: " . $row["nom"]. " - Description: " . $row["description"]. "<br>";
+         }
+      } else {
+         echo "0 results";
+      }
      ?>
   </body>
 </html>

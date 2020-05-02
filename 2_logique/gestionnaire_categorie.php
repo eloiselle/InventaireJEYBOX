7ba6e8deac98597b04ee32dc830ff469aa13dc60
Yannick Plourde <yann62plourde@gmail.com>
2020-05-02 18:09:54 -0400

    Changes to gestionnaire and controllers added
    Changes to gestionnaire and controllers added

--- 2_logique/gestionnaire_categorie.php
+++ 2_logique/gestionnaire_categorie.php
@@ -1,65 +1,65 @@
 <?php
 
   //Gestion des categories
-  public categorie creerCategorie(idCategorie) {
+  public function creerCategorie($idCategorie) {
 
-        categorie uneCategorie = new categorie();
-        Object[] infoCategorie = moteurRBD.selectCategorie(idCategorie);
+        $uneCategorie = new categorie();
+        $infoCategorie = moteurRBD.selectCategorie($idCategorie);
 
-        if (infoCategorie == null) {
+        if ($infoCategorie == null) {
             return null;
         }
 
-        $this-> uneCategorie.set_id(infoCategorieinfoCategorie[0].toString());
-        $this-> uneCategorie.set_nom(infoCategorie[1]);
-        $this-> uneCategorie.set_description(infoCategorie[2]);
+        $uneCategorie.set_id($infoCategorie[0].toString());
+        $uneCategorie.set_nom($infoCategorie[1]);
+        $uneCategorie.set_description($infoCategorie[2]);
 
-        return uneCategorie;
+        return $uneCategorie;
     }
 
-    public Object[] obtenirCategorie(String idCategorie) {
-        Object[] infoCategorie = new Object[3];
-        categorie uneCategorie = creerCategorie(idCategorie);
-        if (uneCategorie == null) {
+    public function obtenirCategorie($idCategorie) {
+        $infoCategorie = new [3];
+        $uneCategorie = creerCategorie($idCategorie);
+        if ($uneCategorie == null) {
             return null;
         }
 
-        infoCategorie[0] = $this-> uneCategorie.get_id();
-        infoCategorie[1] = $this-> uneCategorie.get_nom();
-        infoCategorie[2] = $this-> uneCategorie.get_description();
+        $infoCategorie[0] = $uneCategorie.get_id();
+        $infoCategorie[1] = $uneCategorie.get_nom();
+        $infoCategorie[2] = $uneCategorie.get_description();
 
-        return infoCategorie;
+        return $infoCategorie;
     }
 
     //Gestion des sous-categories
-    public sous_categorie_article creerSC(idSousCategorie) {
+    public function creerSC($idSousCategorie) {
 
-          sous_categorie_article uneSC = new sous_categorie_article();
-          Object[] infoSC = moteurRBD.selectSC(idSousCategorie);
+          $uneSC = new sous_categorie_article();
+          $infoSC = moteurRBD.selectSC($idSousCategorie);
 
-          if (infoSC == null) {
+          if ($infoSC == null) {
               return null;
           }
 
-          $this-> uneSC.set_id(infoSC[0].toString());
-          $this-> uneSC.set_nom(infoSC[1]);
-          $this-> uneSC.set_description(infoSC[2]);
-          $this-> uneSC.set_id_categorie(infoSC[3].toString());
+          $uneSC.set_id($infoSC[0].toString());
+          $uneSC.set_nom($infoSC[1]);
+          $uneSC.set_description($infoSC[2]);
+          $uneSC.set_id_categorie($infoSC[3].toString());
 
-          return uneSC;
+          return $uneSC;
       }
 
-      public Object[] obtenirSC(String idSousCategorie) {
-          Object[] infoSC = new Object[8];
-          sous_categorie_article uneSC = creerSC(idSousCategorie);
-          if (uneSC == null) {
+      public function obtenirSC($idSousCategorie) {
+          $infoSC = new [8];
+          $uneSC = creerSC($idSousCategorie);
+          if ($uneSC == null) {
               return null;
           }
 
-          infoSC[0] = $this-> uneSC.get_id();
-          infoSC[1] = $this-> uneSC.get_nom();
-          infoSC[2] = $this-> uneSC.get_description();
-          infoSC[3] = $this-> uneSC.get_id_categorie();
+          $infoSC[0] = $uneSC.get_id();
+          $infoSC[1] = $uneSC.get_nom();
+          $infoSC[2] = $uneSC.get_description();
+          $infoSC[3] = $uneSC.get_id_categorie();
 
-          return infoSC;
+          return $infoSC;
       }
--- 2_logique/gestionnaires_utilisateur.php
+++ 2_logique/gestionnaires_utilisateur.php
@@ -1,108 +1,108 @@
 <?php
 
   //Gestion des permissions
-  public permission creerPermission(idPermission) {
+  public function creerPermission($idPermission) {
 
-        permission unePermission = new permission();
-        Object[] infoPermission = moteurRBD.selectPermission(idPermission);
+        $unePermission = new permission();
+        $infoPermission = moteurRBD.selectPermission($idPermission);
 
-        if (infoPermission == null) {
+        if ($infoPermission == null) {
             return null;
         }
 
-        $this-> unePermission.set_id(infoPermission[0].toString());
-        $this-> unePermission.set_nom(infoPermission[1]);
+        $unePermission.set_id($infoPermission[0].toString());
+        $unePermission.set_nom($infoPermission[1]);
 
-        return unePermission;
+        return $unePermission;
     }
 
-    public Object[] obtenirPermission(String idPermission) {
-        Object[] infoPermission = new Object[2];
-        permission unePermission = creerPermission(idPermission);
-        if (unePermission == null) {
+    public function obtenirPermission($idPermission) {
+        $infoPermission = new [2];
+        $unePermission = creerPermission($idPermission);
+        if ($unePermission == null) {
             return null;
         }
 
-        infoPermission[0] = $this-> unePermission.get_id();
-        infoPermission[1] = $this-> unePermission.get_nom();
+        $infoPermission[0] = $unePermission.get_id();
+        $infoPermission[1] = $unePermission.get_nom();
 
-        return infoPermission;
+        return $infoPermission;
     }
 
     //Gestion des utilisateurs
-    public utilisateur creerUtilisateur(nomUtilisateur) {
+    public function creerUtilisateur($nomUtilisateur) {
 
-          utilisateur unUtilisateur = new utilisateur();
-          Object[] infoUtilisateur = moteurRBD.selectUtilisateur(nomUtilisateur);
+          $unUtilisateur = new utilisateur();
+          $infoUtilisateur = moteurRBD.selectUtilisateur($nomUtilisateur);
 
-          if (infoUtilisateur == null) {
+          if ($infoUtilisateur == null) {
               return null;
           }
 
-          $this-> unUtilisateur.set_nom_utilisateur(infoUtilisateur[0]);
-          $this-> unUtilisateur.set_mot_de_passe(infoUtilisateur[1]);
-          $this-> unUtilisateur.set_prenom(infoUtilisateur[2]);
-          $this-> unUtilisateur.set_nom(infoUtilisateur[3]);
-          $this-> unUtilisateur.set_courriel(infoUtilisateur[4]);
-          $this-> unUtilisateur.set_telephone(infoUtilisateur[5]);
-          $this-> unUtilisateur.set_naissance((Date) infoUtilisateur[6]);
-          $this-> unUtilisateur.set_id_permission(infoUtilisateur[7].toString());
+          $unUtilisateur.set_nom_utilisateur($infoUtilisateur[0]);
+          $unUtilisateur.set_mot_de_passe($infoUtilisateur[1]);
+          $unUtilisateur.set_prenom($infoUtilisateur[2]);
+          $unUtilisateur.set_nom($infoUtilisateur[3]);
+          $unUtilisateur.set_courriel($infoUtilisateur[4]);
+          $unUtilisateur.set_telephone($infoUtilisateur[5]);
+          $unUtilisateur.set_naissance((Date) $infoUtilisateur[6]);
+          $unUtilisateur.set_id_permission($infoUtilisateur[7].toString());
 
-          return unUtilisateur;
+          return $unUtilisateur;
       }
 
-      public Object[] obtenirUtilisateur(String nomUtilisateur) {
-          Object[] infoUtilisateur = new Object[8];
-          utilisateur unUtilisateur = creerUtilisateur(nomUtilisateur);
-          if (unUtilisateur == null) {
+      public function obtenirUtilisateur($nomUtilisateur) {
+          $infoUtilisateur = new [8];
+          $unUtilisateur = creerUtilisateur($nomUtilisateur);
+          if ($unUtilisateur == null) {
               return null;
           }
 
-          infoUtilisateur[0] = $this-> unUtilisateur.get_nom_utilisateur();
-          infoUtilisateur[1] = $this-> unUtilisateur.get_mot_de_passe();
-          infoUtilisateur[2] = $this-> unUtilisateur.get_prenom();
-          infoUtilisateur[3] = $this-> unUtilisateur.get_nom();
-          infoUtilisateur[4] = $this-> unUtilisateur.get_courriel();
-          infoUtilisateur[5] = $this-> unUtilisateur.get_telephone();
-          infoUtilisateur[6] = $this-> unUtilisateur.get_naissance();
-          infoUtilisateur[7] = $this-> unUtilisateur.get_id_permission();
+          $infoUtilisateur[0] = $unUtilisateur.get_nom_utilisateur();
+          $infoUtilisateur[1] = $unUtilisateur.get_mot_de_passe();
+          $infoUtilisateur[2] = $unUtilisateur.get_prenom();
+          $infoUtilisateur[3] = $unUtilisateur.get_nom();
+          $infoUtilisateur[4] = $unUtilisateur.get_courriel();
+          $infoUtilisateur[5] = $unUtilisateur.get_telephone();
+          $infoUtilisateur[6] = $unUtilisateur.get_naissance();
+          $infoUtilisateur[7] = $unUtilisateur.get_id_permission();
 
-          return infoUtilisateur;
+          return $infoUtilisateur;
       }
 
       //Gestion des contacts d'urgence
-      public contact_urgence creerContactUrgence(idContactU) {
+      public function creerContactUrgence($idContactU) {
 
-            contact_urgence unContact = new contact_urgence();
-            Object[] infoContact = moteurRBD.selectContactUrgence(idContactU);
+            $unContact = new contact_urgence();
+            $infoContact = moteurRBD.selectContactUrgence($idContactU);
 
-            if (infoContact == null) {
+            if ($infoContact == null) {
                 return null;
             }
 
-            $this-> unContact.set_id(infoContact[0].toString());
-            $this-> unContact.set_prenom(infoContact[1]);
-            $this-> unContact.set_nom(infoContact[2]);
-            $this-> unContact.set_relation(infoContact[3]);
-            $this-> unContact.set_telephone(infoContact[4]);
-            $this-> unContact.set_nom_utilisateur(infoContact[5]);
+            $unContact.set_id($infoContact[0].toString());
+            $unContact.set_prenom($infoContact[1]);
+            $unContact.set_nom($infoContact[2]);
+            $unContact.set_relation($infoContact[3]);
+            $unContact.set_telephone($infoContact[4]);
+            $unContact.set_nom_utilisateur($infoContact[5]);
 
-            return unContact;
+            return $unContact;
           }
 
-        public Object[] obtenirContactUrgence(String idContactU) {
-            Object[] infoContact = new Object[6];
-            contact_urgence unContact = creerContactUrgence(idContactU);
-            if (unContact == null) {
+        public function obtenirContactUrgence($idContactU) {
+            $infoContact = new [6];
+            $unContact = creerContactUrgence($idContactU);
+            if ($unContact == null) {
                 return null;
             }
 
-            infoContact[0] = $this-> unContact.get_id();
-            infoContact[1] = $this-> unContact.get_prenom();
-            infoContact[2] = $this-> unContact.get_nom();
-            infoContact[3] = $this-> unContact.get_relation();
-            infoContact[4] = $this-> unContact.get_telephone();
-            infoContact[5] = $this-> unContact.get_nom_utilisateur();
+            $infoContact[0] = $unContact.get_id();
+            $infoContact[1] = $unContact.get_prenom();
+            $infoContact[2] = $unContact.get_nom();
+            $infoContact[3] = $unContact.get_relation();
+            $infoContact[4] = $unContact.get_telephone();
+            $infoContact[5] = $unContact.get_nom_utilisateur();
 
-            return infoContact;
+            return $infoContact;
           }
